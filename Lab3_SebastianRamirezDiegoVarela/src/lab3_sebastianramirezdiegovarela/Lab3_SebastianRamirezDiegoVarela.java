95e05a09d1ee226fae23814b010b242618e0278d
Sebas-Ramirez <ramirez.sebaas@gmail.com>
2020-02-07 14:50:43 -0600

    Principio main

--- Lab3_SebastianRamirezDiegoVarela/src/lab3_sebastianramirezdiegovarela/Lab3_SebastianRamirezDiegoVarela.java
+++ Lab3_SebastianRamirezDiegoVarela/src/lab3_sebastianramirezdiegovarela/Lab3_SebastianRamirezDiegoVarela.java
@@ -1,7 +1,24 @@
 package lab3_sebastianramirezdiegovarela;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Lab3_SebastianRamirezDiegoVarela {
     public static void main(String[] args) {
-       
+        Scanner input = new Scanner(System.in);
+        ArrayList equipos = new ArrayList();
+        ArrayList personal = new ArrayList();
+        System.out.print("1. Crear Equipo\n"
+                + "2. Modificar Equipo\n"
+                + "3. Eliminar Equipo\n"
+                + "Ingrese opcion: ");
+        switch (input.nextInt()) {
+            case 1:
+                
+                break;
+            default:
+                throw new AssertionError();
+        }
     }
     
 }
