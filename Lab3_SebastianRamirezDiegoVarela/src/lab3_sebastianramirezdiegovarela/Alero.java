46e7490aee5049539b554381eb33e929be260527
Diego Varela <diegocrash124@gmail.com>
2020-02-07 14:04:30 -0600

    Se crearon los otros tipode jugador con su altura

---
+++ Lab3_SebastianRamirezDiegoVarela/src/lab3_sebastianramirezdiegovarela/Alero.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3_sebastianramirezdiegovarela;
+public class Alero extends Jugador {
+
+    public Alero(int camiseta, int tiroDe3, int defensa, int tiroDeMedia, int rebote, int bandeja, int posteo, double altura) {
+        super(camiseta, tiroDe3, defensa, tiroDeMedia, rebote, bandeja, posteo, 2.05);
+    }
+    
+}
