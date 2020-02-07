8bd026d68b4a9c469a34b861e0c7a16eada6886c
Diego Varela <diegocrash124@gmail.com>
2020-02-07 14:12:32 -0600

    Se crea el dueño del equipo

---
+++ Lab3_SebastianRamirezDiegoVarela/src/lab3_sebastianramirezdiegovarela/Dueño.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3_sebastianramirezdiegovarela;
+
+public class Dueño extends Persona{
+
+private long netw;    
+private String nacimiento;
+
+public Dueño() {
+    super();
+    }
+
+    public Dueño(long netw, String nacimiento) {
+        this.netw = netw;
+        this.nacimiento = nacimiento;
+    }
+
+}
